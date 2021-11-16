5dacf70ac161a1b409f2e952cabb007e99bd267c
Adam B <el-mapache@github.com>
2021-11-16 08:20:02 -0500

    Ignore git dir

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 for i in .[^.]*; do
+    if [[ -d "$i" ]]; then
+      continue
+    fi
+
     ln -s $i /workspaces/.codespaces/.persistedshare/dotfiles/$i
     source /workspaces/.codespaces/.persistedshare/dotfiles/$i
 done
