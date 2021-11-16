10ec21fddbe6e6238ede106414f2312e8829413b
Adam B <el-mapache@github.com>
2021-11-16 08:42:55 -0500

    once again

--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@ for i in .[^.]*; do
       continue
     fi
 
-    ln -s $i /workspaces/.codespaces/.persistedshare/dotfiles/$i
+    if [[ -f "/workspaces/.codespaces/.persistedshare/dotfiles/$i" ]]; then
+      echo "symlink exists"
+    else
+      ln -s $i /workspaces/.codespaces/.persistedshare/dotfiles/$i
+    fi
     source /workspaces/.codespaces/.persistedshare/dotfiles/$i
 done
